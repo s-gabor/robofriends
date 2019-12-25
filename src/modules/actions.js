b4e56f6edf17fa70cafebc53d93939ce180b020a
s-gabor <ing.gabor@yahoo.com>
2019-12-25 12:33:35 +0200

    Add custom users

--- src/modules/actions.js
+++ src/modules/actions.js
@@ -15,8 +15,28 @@ export const searchAction = (text) => {
 export const requestAction = () => (dispatch) => {
     dispatch({ type: REQUEST_ROBOTS_PENDING });
     
+    const customUsers = [
+        {
+            id: 1000,
+            name: 'Ali'
+        },
+        {
+            id: 1001,
+            name: 'Ale'
+        },
+        {
+            id: 1002,
+            name: 'Ane'
+        }
+    ];
+
     fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
-        .then(users => dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: users }))
+        .then(users => {
+            for (let user of customUsers) {
+                users.push({ id: user.id, name: user.name, email: user.name + '@mail.com' });
+            };            
+            dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: users })
+        })
         .catch(error => dispatch({ type: REQUEST_ROBOTS_FAILED, payload: error }))
 }
