ef44ca7b760b120973f32d28599d0c48279c7137
s-gabor <ing.gabor@yahoo.com>
2019-12-25 09:04:49 +0200

    Add label - to improve Accessibility(77)

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,12 +3,14 @@ import React from 'react';
 const SearchBox = (props) => {
     return(
         <div className='form-inline justify-content-center pt-3 pb-3'>
-            <input
-                className='form-control'
-                type='search' 
-                placeholder='Search robots'
-                onChange={props.onSearchChange}
-            />
+            <label>
+                <input
+                    className='form-control'
+                    type='search'
+                    placeholder='Search robots'
+                    onChange={props.onSearchChange}
+                />
+            </label>
         </div>
     )
 }
