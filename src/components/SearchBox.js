4ed3f2876d94bb6b7db03188a192ab2b02f677d8
s-gabor <ing.gabor@yahoo.com>
2019-12-25 09:16:22 +0200

    Change labal tag to area-lanel - to improve aaccessibility(77)

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,14 +3,13 @@ import React from 'react';
 const SearchBox = (props) => {
     return(
         <div className='form-inline justify-content-center pt-3 pb-3'>
-            <label>
-                <input
-                    className='form-control'
-                    type='search'
-                    placeholder='Search robots'
-                    onChange={props.onSearchChange}
-                />
-            </label>
+            <input
+                area-label='Search Robots'
+                className='form-control'
+                type='search' 
+                placeholder='Search robots'
+                onChange={props.onSearchChange}
+            />
         </div>
     )
 }
