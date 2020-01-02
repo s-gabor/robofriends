ddaca6909869146e22b43cbf73b1f0f613ed6060
s-gabor <ing.gabor@yahoo.com>
2020-01-02 15:08:05 +0200

    First manual test

---
+++ src/components/Card.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Card from './Card';
+
+// shuld be inside a setupTests.js file inside containers
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
+
+it('expect to render Card component', () => {
+    const wraper = shallow(<Card />);
+    expect(wraper).toHaveLength(1);
+});
