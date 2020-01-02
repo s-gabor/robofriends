401f4a91f2def3312a070dae90c02aa60e79d176
s-gabor <ing.gabor@yahoo.com>
2020-01-02 18:47:34 +0200

    Wrap App inside Provider for tests as well, fix test accoding to Snapshot Testing with Jest from jestjs.io

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { shallow } from 'enzyme';
 import Card from './Card';
+import renderer from 'react-test-renderer';
 
-// shuld be inside a setupTests.js file inside containers
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-configure({ adapter: new Adapter() });
-
-it('expect to render Card component', () => {
-    const wraper = shallow(<Card />);
-    expect(wraper).toMatchSnapshot();
+it('renders Card component correctly', () => {
+  const card = renderer
+    .create(<Card />)
+    .toJSON();
+  expect(card).toMatchSnapshot();
 });
--- src/containers/App.test.js
+++ src/containers/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { searchReducer, requestReducer } from '../modules/reducers';
+
+const rootReducer = combineReducers({ searchReducer, requestReducer });
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
